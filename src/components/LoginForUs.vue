8acad27e095099a5bab79ae702c6fef925726a38
imgiang <hoahonggai1199@gmail.com>
2021-12-23 20:26:33 +0700

    header and login

---
+++ src/components/LoginForUs.vue
@@ -0,0 +1,166 @@
+<template>
+  <body>
+    <!-- <img class="img img__login_background" src="src\assets\login_backgroung.png" alt=""> -->
+    <!-- Navbar -->
+    <div class="nav_login">
+      <ul class="nav_login-list nav_login-list-one">
+        <li class="nav_login-item">ESSME</li>
+      </ul>
+      <ul class="nav_login-list nav_login-list-second">
+        <li class="nav_login-item">Home</li>
+        <li class="nav_login-item">Expert</li>
+        <li class="nav_login-item">Languages</li>
+        <li class="nav_login-item">
+          <img class="img img__login_icon--small_size" src="src\assets\facebook.png" alt="" />
+        </li>
+        <li class="nav_login-item">
+          <img class="img img__login_icon--small_size" src="src\assets\twitter.png" alt="" />
+        </li>
+        <li class="nav_login-item">
+          <img class="img img__login_icon--small_size" src="src\assets\instagram.png" alt="" />
+        </li>
+      </ul>
+    </div>
+    <!-- main box chinh -->
+    <div class="login_main_center-layout">
+      <h3 class="center-layout__item">Sign In</h3>
+
+      <div class="signIn-google">
+        <img
+          class="img img__login_icon--large_size icon-google"
+          src="https://gonline.vn/wp-content/uploads/2019/03/google-icon-1-2-1360x1360.png"
+          alt=""
+        />
+        <p class="gg-text-signinWith">Sign In With Google</p>
+      </div>
+    </div>
+    <!-- footer nav -->
+    <div class="nav_login_bottom">
+      <ul class="nav_login_bottom-list">
+        <li class="nav_login_bottom-item">ESSME</li>
+        <li class="nav_login_bottom-item">ABOUT US</li>
+        <li class="nav_login_bottom-item">BLOG</li>
+      </ul>
+    </div>
+  </body>
+</template>
+<script lang="ts">
+// import * as Module from 'module';
+</script>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+  background-image: url(../assets/login_background.png);
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
+.img__login_background {
+  width: 100%;
+  height: 100%;
+}
+.nav_login-list {
+  list-style: none;
+  padding-left: 0;
+  // margin: auto 16px;
+
+  // margin: 0 8px;
+}
+.nav_login {
+  position: relative;
+  width: 100%;
+  top: 23px;
+  min-height: 50px;
+  display: inline-flex;
+  justify-content: space-between;
+}
+.nav_login-item {
+  color: rgba(0, 0, 0, 1);
+  list-style: none;
+  margin: 0 8px;
+  display: inline-flex;
+  // align-self: center;
+}
+.nav_login-item:first-child {
+  margin-left: 150px;
+}
+.nav_login-list-one {
+  font-size: 1.2rem;
+}
+.nav_login-item:last-child {
+  margin-right: 110px;
+}
+.img__login_icon--small_size {
+  width: 18px;
+  height: 20px;
+}
+.img__login_icon--large_size {
+  width: 40px;
+  height: 40px;
+}
+.icon
+// BOTTOM NAV
+.nav_login_bottom {
+  width: 100%;
+  position: flex;
+  bottom: 4mm;
+  display: flex;
+}
+.nav_login_bottom-list {
+  list-style: none;
+  display: inline-flex;
+}
+.nav_login_bottom-item {
+  margin: 0 8px;
+}
+.login_main_center-layout {
+  background-color: rgb(192, 227, 194);
+  flex-direction: column;
+  margin: 70px auto 0 auto;
+  padding: 32px 0;
+  max-width: 400px;
+  height: 250px;
+  display: flex;
+  border-radius: 30px;
+}
+.center-layout__item:first-child {
+  margin: 0 auto;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 25px;
+  align-self: flex-start;
+}
+.center-layout__item:last-child {
+  margin: 0 auto;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 25px;
+  align-self: flex-start;
+}
+.signIn-google {
+  height: 30%;
+  width: 75%;
+  margin-top: 10%;
+  background-color: #90aaa1;
+  display: flex;
+  align-self: center;
+  border-radius: 20px;
+  justify-content: center;
+}
+.icon-google {
+  align-self: center;
+}
+.gg-text-signinWith {
+  width: 60%;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+}
+</style>
--- src/components/layouts/homeFragment/Header.vue
+++ src/components/layouts/homeFragment/Header.vue
@@ -1,33 +1,37 @@
 <template>
-<img class="background" src="src\assets\carousel.png">
-<div class="navbar top">
-  <p class="logo">ESSME</p>
-  <div class="dropdown item">
-    <button class="dropbtn">Login</button>
-  </div>
-  <div class="dropdown item">
-    <button class="dropbtn">Languages</button>
-    <div class="dropdown-content border-radius-up border-radius-down">
-      <a class="border-radius-up">English</a>
-      <a class="border-radius-down">Tiếng Việt</a>
+  <img class="background" src="src\assets\carousel.png" />
+  <div class="navbar top">
+    <p class="logo">ESSME</p>
+    <div class="dropdown item">
+      <button class="dropbtn">Login</button>
+    </div>
+    <div class="dropdown item">
+      <button class="dropbtn">Languages</button>
+      <div class="dropdown-content border-radius-up border-radius-down">
+        <a class="border-radius-up">English</a>
+        <a class="border-radius-down">Tiếng Việt</a>
+      </div>
+    </div>
+    <div class="dropdown item">
+      <button class="dropbtn">Expert</button>
+      <div class="dropdown-content border-radius-up border-radius-down">
+        <a class="border-radius-up">FQAs</a>
+        <a>Order Expert</a>
+        <a class="border-radius-down">Expert Info</a>
+      </div>
+    </div>
+    <div class="dropdown item">
+      <button class="dropbtn">Home</button>
     </div>
   </div>
-  <div class="dropdown item">
-    <button class="dropbtn">Expert</button>
-    <div class="dropdown-content border-radius-up border-radius-down">
-      <a class="border-radius-up">FQAs</a>
-      <a>Order Expert</a>
-      <a class="border-radius-down">Expert Info</a>
-    </div>
-  </div>
-  <div class="dropdown item">
-    <button class="dropbtn">Home</button>
-  </div>
-</div>
 </template>
 
 <style>
-.background{
+:root {
+  --maincolor: #b9cec7;
+  --btncolor: #e6f0ec;
+}
+.background {
   height: 548px;
   width: 100%;
 }
@@ -36,16 +40,16 @@
   position: absolute;
   top: 0px;
 }
-.border-radius-up{
+.border-radius-up {
   /* border-top-left-radius: 5px; */
   border-top-right-radius: 5px;
 }
-.border-radius-down{
+.border-radius-down {
   border-bottom-left-radius: 5px;
   border-bottom-right-radius: 5px;
 }
 .dropbtn {
-  background-color: #EFB0C9;
+  background-color: var(--maincolor);
   color: black;
   font-weight: normal;
   padding: 16px;
@@ -66,12 +70,11 @@
 .dropdown-content {
   display: none;
   position: absolute;
-  background-color: #F8DAE9;
+  background-color: var(--btncolor);
   min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
 }
-
 
 /* / * Liên kết bên trong danh sách thả xuống * / */
 .dropdown-content a {
@@ -83,7 +86,7 @@
 
 /* / * Thay đổi định dạng của liên kết thả xuống khi di chuột qua * / */
 .dropdown-content a:hover {
-  background-color: #EFB0C9;
+  background-color: var(--btncolor);
   color: white;
 }
 
@@ -94,40 +97,40 @@
 
 /* / * Thay đổi màu nền của nút thả xuống khi nội dung được hiển thị * / */
 .dropdown:hover .dropbtn {
-  background-color: #F8DAE9;
+  background-color: var(--btncolor);
 }
 /*  */
-.color{
-  background-color: #F8DAE9;
+.color {
+  background-color: var(--btncolor);
 }
-.font{
+.font {
   font-family: Arial, Helvetica, sans-serif;
   font-size: larger;
   color: #000000;
 }
-.item{
+.item {
   float: right;
   font-family: Arial, Helvetica, sans-serif;
   margin-left: 0cm;
   margin-right: 1cm;
   color: #000000;
 }
-.logo{
-    float: left;
-    margin-top: 0.45cm;
-    margin-left: 1cm;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    color: #000000;
-  }
-  .navbar{
-    margin: 0 auto; /* Căn giữa block element */
-    margin-top: 0.5cm;
-    width:  95%;
-    height: 52px;
-    background: #EFB0C9;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;
-  }
+.logo {
+  float: left;
+  margin-top: 0.45cm;
+  margin-left: 1cm;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  color: #000000;
+}
+.navbar {
+  margin: 0 auto; /* Căn giữa block element */
+  margin-top: 0.5cm;
+  width: 95%;
+  height: 52px;
+  background: var(--maincolor);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+}
 </style>
