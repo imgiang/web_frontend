157fc5ee39c6a89232e825675dbecaa89d02612f
luanbt21 <luanbt21@gmail.com>
2021-12-26 17:07:30 +0700

    demo search in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <SearchExperts />
   <MainHome />
   <Footer />
 </template>
@@ -6,6 +7,7 @@
 <script setup lang="ts">
 import MainHome from './components/Main.vue'
 import Footer from './components/Footer.vue'
+import SearchExperts from './components/SearchExperts.vue'
 </script>
 
 <style>
