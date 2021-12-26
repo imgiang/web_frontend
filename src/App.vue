dd37e73929ba786f796202b11b12292efcd74008
luanbt21 <luanbt21@gmail.com>
2021-12-26 22:02:18 +0700

    fetch src

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <template>
-  <BaseHeader />
-  <BackupRestore />
-  <label for="search">Search</label>
-  <input id="search" v-model="search" @input="getSuggest" />
-  <div v-for="expert in experts" :key="expert._id">
-    <img :src="expert._source.thumbnail ? expert._source.thumbnail : avatar" :alt="expert._source.name + ' image'" />
-    {{ expert._source.name }}
-    <button @click="deleteExpertById(expert._id)">Delete</button>
-  </div>
+  <MainHome />
+  <Footer />
 </template>
 
 <script setup lang="ts">
-import BaseHeader from './components/layouts/BaseHeader.vue'
-import BackupRestore from '~/views/BackupRestore.vue';
-import { ref, onMounted } from 'vue'
-
-import { getExperts, searchExperts, deleteExpertById } from '~/api/Experts'
-import Expert from './model/Expert'
-import avatar from '/avatar.jpg'
-
-const experts = ref<Expert[]>([])
-const search = ref('')
-
-const getSuggest = async () => {
-  console.log(search.value)
-  experts.value = await searchExperts(search.value)
-}
-
-onMounted(async () => {
-  experts.value = await getExperts()
-})
+import MainHome from './components/Main.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <style>
@@ -37,10 +13,7 @@ onMounted(async () => {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-}
-.element-plus-logo {
-  width: 50%;
 }
 </style>
