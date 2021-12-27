dd12f74618ba32706a609bbeccd7867d1ca36aa5
luanbt21 <luanbt21@gmail.com>
2021-12-27 22:10:30 +0700

    electron app config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: path.resolve(__dirname, "./dist"),
   resolve: {
     alias: {
       "~/": `${path.resolve(__dirname, "src")}/`,
